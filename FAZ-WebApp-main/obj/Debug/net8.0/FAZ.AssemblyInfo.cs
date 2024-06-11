5ca7553b91ab630cdbedc114a76a9eb9d41bd3ae
Chiyesu <jayjaychiyesu@gmail.com>
2024-06-11 18:55:22 +0200

    made changes

--- FAZ-WebApp-main/obj/Debug/net8.0/FAZ.AssemblyInfo.cs
+++ FAZ-WebApp-main/obj/Debug/net8.0/FAZ.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FAZ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07476053db4351335ae72381eeea5280701b090e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+776f8be355515b2f2a7aaec8b5d9cb1d97760428")]
 [assembly: System.Reflection.AssemblyProductAttribute("FAZ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FAZ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
