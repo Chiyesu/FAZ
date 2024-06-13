6c114ac00e9bdbb9ed838dbda2ed45936a52a656
Chiyesu <jayjaychiyesu@gmail.com>
2024-06-13 21:38:34 +0200

    working on the view matches page

--- FAZ-WebApp-main/obj/Debug/net8.0/FAZ.AssemblyInfo.cs
+++ FAZ-WebApp-main/obj/Debug/net8.0/FAZ.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FAZ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a567b3d4e01d63778043ae40bd495d53d7b07ffb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9ff6e07b222953c48fc5273f2ccffaeb72345bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("FAZ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FAZ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
