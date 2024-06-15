21a9d934da4f9d2ed3ccc08654210e4279d483c5
Konayuma <sepokonayuma@gmail.com>
2024-06-15 20:20:05 +0200

    gitignore

--- FAZ-WebApp-main/obj/Debug/net8.0/FAZ.AssemblyInfo.cs
+++ FAZ-WebApp-main/obj/Debug/net8.0/FAZ.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FAZ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1679d0bf5b1240a8dd01b77bd478aae72a120b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4509b10c239068740a558631ef5e262ad62edf5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FAZ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FAZ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
