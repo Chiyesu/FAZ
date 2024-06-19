ea568608e50809b9aa556dbb12745ee7d8b30b5b
Konayuma <sepokonayuma@gmail.com>
2024-06-19 07:18:39 +0200

    separated the referees

--- FAZ-WebApp-main/Models/Match.cs
+++ FAZ-WebApp-main/Models/Match.cs
@@ -1,8 +1,14 @@
-public class Match{
-    public int MatchId { get; set; }
-    public int HostTeamId { get; set; }
-    public int GuestTeamId { get; set; }
-    public DateOnly DatePlayed { get; set; }
-    public string FinalResult { get; set; }
-    public int RefereeId { get; set; }
+namespace Data
+{
+    public class Match
+    {
+        public int MatchId { get; set; }
+        public int HostTeamId { get; set; }
+        public int GuestTeamId { get; set; }
+        public string FinalResult { get; set; }
+        public int RefereeId { get; set; } 
+        public int AssistantReferee1Id { get; set; } 
+        public int AssistantReferee2Id { get; set; }
+        public DateTime DatePlayed { get; set; }
+    }
 }
